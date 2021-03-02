8fb48c5989c08669d78faa409e9c11ec2f03a117
Andrew Herron <thespyder@programmer.net>
2021-03-02 14:38:33 +1000

    Hooked up the model loader on startup with DOM as the fallback Exposed editor.model property

--- modules/tinymce/src/core/main/ts/init/InitContentBody.ts
+++ modules/tinymce/src/core/main/ts/init/InitContentBody.ts
@@ -20,6 +20,7 @@ import Formatter from '../api/Formatter';
 import DomParser, { DomParserSettings } from '../api/html/DomParser';
 import AstNode from '../api/html/Node';
 import Schema from '../api/html/Schema';
+import ModelManager from '../api/ModelManager';
 import * as Settings from '../api/Settings';
 import UndoManager from '../api/UndoManager';
 import Delay from '../api/util/Delay';
@@ -309,6 +310,15 @@ const loadContentCss = (editor: Editor, css: string[]) => {
   Promise.all(makeStylesheetLoadingPromises(editor, css, fontCss)).then(loaded).catch(loaded);
 };
 
+const initModel = (editor: Editor) => {
+  const modelSetting = Settings.getModel(editor);
+
+  const model = Type.isString(modelSetting) ? modelSetting : 'dom';
+
+  const Model = ModelManager.get(model);
+  editor.model = new Model(editor, ModelManager.urls[model]);
+};
+
 const preInit = (editor: Editor, rtcMode: boolean) => {
   const settings = editor.settings, doc = editor.getDoc(), body = editor.getBody();
 
@@ -316,6 +326,8 @@ const preInit = (editor: Editor, rtcMode: boolean) => {
     doc.body.spellcheck = false; // Gecko
     DOM.setAttrib(body, 'spellcheck', 'false');
   }
+
+  initModel(editor);
 
   editor.quirks = Quirks(editor);
 
@@ -454,6 +466,7 @@ const initContentBody = (editor: Editor, skipWrite?: boolean) => {
   Placeholder.setup(editor);
 
   Events.firePreInit(editor);
+
 
   Rtc.setup(editor).fold(() => {
     preInit(editor, false);
